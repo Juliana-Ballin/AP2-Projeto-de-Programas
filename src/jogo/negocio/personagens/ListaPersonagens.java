d44d4f37f8f9f9215df50a393b82527d191fda68
Juliana Ballin <149808117+Juliana-Ballin@users.noreply.github.com>
2024-07-22 07:55:47 -0400

    Update ListaPersonagens.java

--- src/jogo/negocio/personagens/ListaPersonagens.java
+++ src/jogo/negocio/personagens/ListaPersonagens.java
@@ -2,23 +2,19 @@ package jogo.negocio.personagens;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-
 import jogo.persistencia.PersonagemDAO;
 
 public class ListaPersonagens implements Serializable {
-	private static final long serialVersionUID = 3308306968453549378L;
+    private static final long serialVersionUID = 3308306968453549378L;
+    private ArrayList<Personagem> personagens = new ArrayList<>();
+    private static PersonagemDAO personagemDAO = new PersonagemDAO();
 
-	private ArrayList<Personagem> personagens = new ArrayList<Personagem>();
-	
-	private static PersonagemDAO personagemDAO = new PersonagemDAO();
-	
-	public ArrayList<Personagem> getPersonagens(){
-		return this.personagens;
-	}
-	
-	public void salvarPersonagem(Personagem personagem) throws Exception {
-		personagens.add(personagem);
-		personagemDAO.salvar(this);
-	}
-	
+    public ArrayList<Personagem> getPersonagens() {
+        return this.personagens;
+    }
+
+    public void salvarPersonagem(Personagem personagem) throws Exception {
+        personagens.add(personagem);
+        personagemDAO.salvar(this);
+    }
 }
