9eeb4b11da176bc3714ec956b937d28179c06c55
Juliana Ballin <149808117+Juliana-Ballin@users.noreply.github.com>
2024-07-22 07:55:45 -0400

    Update ListaEquipe.java

--- src/jogo/negocio/ListaEquipe.java
+++ src/jogo/negocio/ListaEquipe.java
@@ -2,22 +2,19 @@ package jogo.negocio;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-
 import jogo.persistencia.EquipeDAO;
 
 public class ListaEquipe implements Serializable {
-	private static final long serialVersionUID = 1692973258550795890L;
+    private static final long serialVersionUID = 1692973258550795890L;
+    private ArrayList<Equipe> equipes = new ArrayList<>();
+    private static EquipeDAO equipeDAO = new EquipeDAO();
 
-	private ArrayList<Equipe> equipes = new ArrayList<Equipe>();
-	
-	private static EquipeDAO equipeDAO = new EquipeDAO();
-	
-	public ArrayList<Equipe> getEquipes(){
-		return this.equipes;
-	}
-	
-	public void salvarEquipe(Equipe equipe) throws Exception {
-		equipes.add(equipe);
-		equipeDAO.salvar(this);
-	}
+    public ArrayList<Equipe> getEquipes() {
+        return this.equipes;
+    }
+
+    public void salvarEquipe(Equipe equipe) throws Exception {
+        equipes.add(equipe);
+        equipeDAO.salvar(this);
+    }
 }
